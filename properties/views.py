840bc366a88b15c9100d9bf2dc0fa12b63ff0a5d
Theophilus Frimpong <theophilusfrimpong17@gmail.com>
2026-02-02 00:06:12 +0000

    Add properties app: Implemented property listing view with caching and updated URL routing

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.cache import cache_page
+from .models import Property
 
-# Create your views here.
+
+@cache_page(60 * 15)
+def property_list(request):
+    """
+    View to return all properties.
+    Cached for 15 minutes using Redis.
+    """
+    properties = Property.objects.all()
+    properties_data = [
+        {
+            'id': prop.id,
+            'title': prop.title,
+            'description': prop.description,
+            'price': str(prop.price),
+            'location': prop.location,
+            'created_at': prop.created_at.isoformat(),
+        }
+        for prop in properties
+    ]
+    return JsonResponse(properties_data, safe=False)
